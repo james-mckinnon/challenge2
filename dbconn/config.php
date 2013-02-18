2f6c431d3c8779bade0d801b104ee52ce9afefad
James McKinnon <james.mckinnon@wildfireinternet.co.uk>
2013-02-18 20:18:14 +0000

    Added Boxile for Pagoda, Changed DB connection config options to work with Pagoda

--- dbconn/config.php
+++ dbconn/config.php
@@ -1,10 +1,10 @@
 <?php
 
 $config = array(
-	'HOST' => '' /*Name of Host*/,
-	'DB_NAME' => '' /*Name of Database*/,
-	'DB_USERNAME' => '' /*Database Username*/,
-	'DB_PASSWORD' => '' /*Database Password*/
+	'HOST' => $_SERVER['DB1_HOST'] /*Name of Host*/,
+	'DB_NAME' => $_SERVER['DB1_NAME'] /*Name of Database*/,
+	'DB_USERNAME' => $_SERVER['DB1_USER'] /*Database Username*/,
+	'DB_PASSWORD' => $_SERVER['DB1_PASS'] /*Database Password*/
 );
 
 ?>
